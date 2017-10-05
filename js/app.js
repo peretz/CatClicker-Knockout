aa4c5cbf5eb92cb6696f5e55ff5b26fa35545c88
Peretz <peretzman@gmail.com>
2017-10-05 10:11:06 -0500

    Added initial sample cat model and respective bindings.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 var ViewModel = function() {
 
+    this.name = ko.observable("Pedrito");
+    this.image = ko.observable("images/cat1.jpg");
+    this.clickCount = ko.observable(0);
+
+    this.incrementCounter = function() {
+        this.clickCount(this.clickCount() + 1);
+    };
 }
 
 ko.applyBindings(new ViewModel());
