ac5c829d0c621db0f74c1f06c269496256b90666
Peretz <peretzman@gmail.com>
2017-10-05 13:39:40 -0500

    Added list of cats and allowed every item in the list to be clickable so that it shows the respective cat information detail.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,42 @@
+var initialCats = [
+    {
+        "name" : "Pedrito",
+        "image" : "images/cat1.jpg",
+        "clickCount" : 0,
+        "nicknames": ["Pedrin", "Cosita", "Pirruron"]
+    },
+    {
+        "name" : "Juanito",
+        "image" : "images/cat2.jpg",
+        "clickCount" : 0,
+        "nicknames": ["Juanjo", "Juanin"]
+    },
+    {
+        "name" : "Luis",
+        "image" : "images/cat3.jpg",
+        "clickCount" : 0,
+        "nicknames": ["Luigi", "Luisin"]
+    },
+    {
+        "name" : "Mario",
+        "image" : "images/cat4.jpg",
+        "clickCount" : 0,
+        "nicknames": ["Mariuchis"]
+    },
+    {
+        "name" : "Chuchito",
+        "image" : "images/cat5.jpg",
+        "clickCount" : 0,
+        "nicknames": ["Jesusin"]
+    },
+    {
+        "name" : "Kevin",
+        "image" : "images/cat6.jpg",
+        "clickCount" : 0,
+        "nicknames": ["Kevincin", "Vincho"]
+    }
+]
+
 var Cat = function(cat) {
     this.name = ko.observable(cat.name);
     this.image = ko.observable(cat.image);
@@ -24,16 +63,22 @@ var Cat = function(cat) {
 }
 
 var ViewModel = function() {
-    this.currentCat = ko.observable(new Cat({
-        name: "Pedrito",
-        image: "images/cat1.jpg",
-        clickCount: 0,
-        nicknames: ["Pedrin", "Cosita", "Pirruron"]
-    }));
+
+    this.listOfCats = ko.observableArray([]);
 
     var self = this;
+    initialCats.forEach(function(catEntry) {
+       self.listOfCats.push(new Cat(catEntry)); 
+    });
+
+    this.currentCat = ko.observable(this.listOfCats()[0]);
+
     this.incrementCounter = function() {
         self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+    };
+
+    this.setCurrentCat = function(index) {
+        self.currentCat(self.listOfCats()[index]);
     };
 }
 
